cc8ba50e608e2be729861a9a8aaa8dadae1616d1
foamyguy <foamyguy@gmail.com>
2020-12-19 11:41:53 -0600

    add simpletest. black/pylint. add background_color param. docstrings

--- circuitpython_display_frame.py
+++ circuitpython_display_frame.py
@@ -6,7 +6,8 @@
 `circuitpython_display_frame`
 ================================================================================
 
-CircuitPython displayio widget to create a rounded rectangle frame with text label at the top center.
+CircuitPython displayio widget to create a rounded rectangle frame
+with text label at the top center.
 
 
 * Author(s): Tim C
@@ -21,7 +22,7 @@ Implementation Notes
 
 * Adafruit CircuitPython Display Text:
   https://github.com/adafruit/Adafruit_CircuitPython_Display_Text
-  
+
 * Adafruit CircuitPython Display Shapes:
   https://github.com/adafruit/Adafruit_CircuitPython_Display_Shapes
 
@@ -37,34 +38,66 @@ import terminalio
 from adafruit_display_text import bitmap_label
 from adafruit_display_shapes.roundrect import RoundRect
 
+
 class Frame(displayio.Group):
-    
-    
+    # pylint: disable=too-many-arguments
+    """
+    A rounded rectangle frame with a text label at the top center.
+
+    :param int x: The x-position of the top left corner.
+    :param int y: The y-position of the top left corner.
+    :param int width: The width of the rounded-corner rectangle.
+    :param int height: The height of the rounded-corner rectangle.
+    :param int corner_radius: The radius of the rounded corner.
+    :param str text: Text to display
+    :param Font font: A font class that has ``get_bounding_box`` and ``get_glyph``.
+      Defaults to terminalio.FONT
+    :param outline: The outline of the rounded-corner rectangle. Can be a hex value for a color.
+    :param stroke: Used for the outline. Will not change the outer bound size set by ``width`` and
+                   ``height``.
+    """
+
     def __init__(
         self,
-        width,
-        height,
         x,
         y,
+        width,
+        height,
         corner_radius=10,
         text="Frame",
         font=terminalio.FONT,
         outline=0xFFFFFF,
         text_color=None,
-        stroke=2
+        background_color=0x0,
+        stroke=2,
     ):
         super().__init__(x=x, y=y)
-        
-        roundrect = RoundRect(0, 0, width, height, corner_radius, fill=None, outline=outline, stroke=stroke)
+
+        roundrect = RoundRect(
+            0,
+            0,
+            width,
+            height,
+            corner_radius,
+            fill=None,
+            outline=outline,
+            stroke=stroke,
+        )
         self.append(roundrect)
-        
+
         if outline and not text_color:
             text_color = outline
-        
-        self.label = bitmap_label.Label(font, text=text, color=text_color, background_color=0x0, padding_left=2, padding_right=1)
+
+        self.label = bitmap_label.Label(
+            font,
+            text=text,
+            color=text_color,
+            background_color=background_color,
+            padding_left=2,
+            padding_right=1,
+        )
         self.label.anchor_point = (0.5, 0.5)
-        self.label.anchored_position = (width//2, 0)
-        if self.label.bounding_box[2] * 2 < width - (corner_radius*2):
+        self.label.anchored_position = (width // 2, 0)
+        if self.label.bounding_box[2] * 2 < width - (corner_radius * 2):
             self.label.scale = 2
         self.append(self.label)
-
