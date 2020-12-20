a40559e6a0b3878779e203c25e9896a404114a96
foamyguy <foamyguy@gmail.com>
2020-12-20 07:46:52 -0600

    fix pypi name

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="adafruit-circuitpython-display-frame",
+    name="circuitpython-display-frame",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     description="CircuitPython displayio widget to create a rounded rectangle frame with text label at the top center.",
